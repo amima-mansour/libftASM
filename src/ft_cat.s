59a546bac82a0baa7c4a385f10f41eadba498738
amansour <amansour@MacBook-Air-de-amansour.local>
2019-09-17 18:15:15 +0100

    fixed pb cat

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -6,35 +6,38 @@
 ; ----------------------------------------------------------------------------------------
 
 %define STDOUT 1
-%define BUFFER_SIZE 5
+%define BUFFER_SIZE 1000
 
 section .bss
     buffer resb BUFFER_SIZE
 section .text
     global _ft_cat
-    extern  _ft_puts
+
 _ft_cat:
     push rbp
     mov rbp, rsp
     sub rsp, 16
     
-    mov rbx, rdi
-    lea rsi, [rel buffer]
+    mov [rsp], rdi
 loop_read:
-    mov rax, 0x2000003
+    mov rdi, [rsp]
+    lea rsi, [rel buffer]
     mov rdx, BUFFER_SIZE
+    mov rax, 0x2000003
     syscall
-    cmp rax, 0
-    jle leave
-    mov rdx, rax
+    jc leave						; exit Error (rax is neg, carry flag is set)
+	cmp rax, 0						; check read EOF
+    je leave
 write:
+    mov rdx, rax
     mov rax, 0x2000004
     mov	rdi, STDOUT
+    lea rsi, [rel buffer]
     syscall
-    mov rdi, rbx
+    jc leave						; exit Error (rax is neg, carry flag is set)
+    mov rdi, [rsp]
     jmp loop_read
 leave:
-    mov rax, rdx
     mov rsp, rbp
     pop rbp
     ret
