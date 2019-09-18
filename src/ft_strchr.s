68031cf92c33277994713317ab7d50d73bd51918
amansour <amansour@MacBook-Air-de-amansour.local>
2019-09-18 09:31:27 +0100

    pb strchr fixed

--- src/ft_strchr.s
+++ src/ft_strchr.s
@@ -15,9 +15,11 @@ loop:
     cmp byte[rdi], sil
     je leave
     cmp byte [rdi], 0
-    je leave
+    je res_null
     inc rdi
     jmp loop
+res_null :
+    mov rdi, 0
 leave:
     mov rax, rdi
     mov rsp, rbp
